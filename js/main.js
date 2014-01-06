4c8c66b53b3b62184e40e2c5ba31aa0e326bd1a8
Todd Scheuring <todd@cx.com>
2014-01-06 01:08:55 -0700

    fixing video js code

--- js/main.js
+++ js/main.js
@@ -6,9 +6,10 @@ $(document).ready(function(){
 	$(".play-btn").click(function(){
 		event.preventDefault();
 		$("#modal").removeClass('modal-hide').addClass('modal-show');
+		$('#video1').get(0).play();
 	});
 	$(".modal-bg").click(function(){
 		$("#modal").removeClass('modal-show').addClass('modal-hide');
-		$('#VidPlayer').get(0).stopVideo();
+		$('#video1').get(0).pause();
 	});
 });
